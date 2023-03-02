89a7d91a5001a3211b33a19b8266c62929aca857
阿章 <1377699408@qq.com>
2023-03-02 09:29:57 +0800

    tmux安装

---
+++ _posts/install_tmux.sh
@@ -0,0 +1,18 @@
+sudo yum install -y http://galaxy4.net/repo/galaxy4-release-7-current.noarch.rpm
+sudo yum install -y tmux
+cat > /root/.tmux.conf <<EOF
+set -g allow-rename off
+setw -g mouse on
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+# 文件目录树
+# 切换到侧栏的目录树: <prefix+Tab>
+# 光标移动到侧边栏上: <prefix+Backspace>
+set -g @plugin 'tmux-plugins/tmux-sidebar'
+run '~/.tmux/plugins/tpm/tpm'
+EOF
+yum install -y git
+git clone https://gitee.com/guanvi/tpm  ~/.tmux/plugins/tpm
+git clone https://gitee.com/wuxler/tmux-sensible ~/.tmux/plugins/tpm-sensible
+git clone https://gitee.com/yuruilee/tmux-resurrect  ~/.tmux/plugins/tpm-resurrect
